8e49dc3a792125c1b0a139b45d2d8196bc861f1e
elenilson <sousalele99@icloud.com>
2022-06-08 18:05:25 -0300

    Security BlogPessoa

--- src/main/java/com/generationblogpessoal/model/Postagem.java
+++ src/main/java/com/generationblogpessoal/model/Postagem.java
@@ -39,6 +39,11 @@ public class Postagem {
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
 	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
+	
+	
 	public Long getId() {
 		return id;
 	}
@@ -79,7 +84,13 @@ public class Postagem {
 		this.tema = tema;
 	}
 
-	
+	public Usuario getUsuario() {
+		return this.usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 	
 	
 }
